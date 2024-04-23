49a7870f1a1d920e127e7fea5a3b0011b502778d
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-04-23 08:28:55 +0200

    Update LoginForm.tsx

--- app/(marketing)/login/_components/LoginForm.tsx
+++ app/(marketing)/login/_components/LoginForm.tsx
@@ -23,8 +23,8 @@ export default function LoginForm() {
     const form = useForm<z.infer<typeof loginSchema>>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
-            email: "mahmoudnaggar2002@gmail.com",
-            password: "12345678",
+            email: "",
+            password: "",
         },
     });
     const submitForm = async (values: z.infer<typeof loginSchema>) => {
