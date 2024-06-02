b5dc63f52df90d52195ec67923041be812bdf81c
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-06-02 21:00:31 +0300

    increment

--- components/General/AboutFloatingButton.tsx
+++ components/General/AboutFloatingButton.tsx
@@ -1,13 +1,16 @@
 'use client';
-import { Info } from 'lucide-react'
+import { Info, Link2Icon, LinkIcon, MailIcon, PhoneIcon } from 'lucide-react'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
-
+import parse from 'html-react-parser';
+import Image from 'next/image';
+import { Avatar, AvatarFallback } from '../ui/avatar';
+import Link from 'next/link';
 
 export default function AboutFloatingButton() {
     const handelOpenDialog = () => { }
     return (
-        <div className="group fixed bottom-4 right-4 p-2  flex items-end justify-end w-24 h-24 ">
+        <div className="group fixed bottom-4 right-4 p-2  flex items-end justify-end  ">
             <Dialog>
                 <Tooltip>
                     <TooltipTrigger asChild>
@@ -27,8 +30,51 @@ export default function AboutFloatingButton() {
                             About me
                         </DialogTitle>
                     </DialogHeader>
-                    <div className="flex items-center space-x-2">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nemo, sequi quod rerum corrupti ad voluptatum optio, enim nihil nostrum cumque dolorem doloremque libero, neque laborum impedit? Reiciendis, nulla amet?
+                    <div className="grid gap-6 p-6 sm:p-10">
+                        <div className='flex gap-4 flex-col lg:flex-row'>
+                            <div className="flex flex-col items-center gap-4">
+                                <Image
+                                    className=' rounded-full w-[150] aspect-square object-cover'
+                                    width={100}
+                                    height={100}
+                                    src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?size=626&ext=jpg&ga=GA1.1.1518270500.1717286400&semt=ais_user"
+                                    alt="@username" />
+                                <div className="grid gap-1 text-center">
+                                    <h3 className="text-2xl font-bold">Mahmoud Khaled</h3>
+                                    <p className="text-gray-500 dark:text-gray-400">Software Engineer</p>
+                                </div>
+                            </div>
+                            <div className=" mx-auto lg:mx-0 space-y-4">
+                                <h2 className=' font-bold'>Contact Me</h2>
+                                <div className="flex items-center gap-2">
+                                    <MailIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+                                    <a href='mailto:mahmoudnaggar2002@gmail.com' className="text-gray-500 dark:text-gray-400">mahmoudnaggar2002@gmail.com</a>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <PhoneIcon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+                                    <p className="text-gray-500 dark:text-gray-400">+201145243378</p>
+                                </div>
+                                <div className="flex items-center gap-2">
+                                    <Link2Icon className="h-5 w-5 text-gray-500 dark:text-gray-400" />
+                                    <a
+                                        href="https://portfolio-builder-frontend.vercel.app/portfolio/656e0b5bb0739f8d5367afb6"
+                                        className=" underline text-gray-500 dark:text-gray-400"
+                                        target='__blank'
+                                    >
+
+                                        View Portfolio
+                                    </a>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="grid gap-4 text-center">
+                            <p className="text-sm text-gray-500 dark:text-gray-400">
+                                Hi! I'm Mahmoud Khaled, a software developer specializing in mobile apps and full stack websites. I create innovative, user-friendly applications for iOS and Android, and handle all aspects of web development.
+                                Let's connect and bring your ideas to life!
+                            </p>
+
+                        </div>
                     </div>
 
                 </DialogContent>
